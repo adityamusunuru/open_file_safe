bda95967d156cae4cd7b96b59aa0f10c49d8a560
Aditya Musunuru <aditya.musunuru@jungleegames.com>
2023-07-28 15:17:25 +0530

    upgraded to make it compatible with flutter 3.10.6

--- example/ios/Flutter/flutter_export_environment.sh
+++ example/ios/Flutter/flutter_export_environment.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=C:\flutter"
-export "FLUTTER_APPLICATION_PATH=C:\Users\mahmu\Documents\Arka Packages\open_file\example"
+export "FLUTTER_ROOT=/Users/aditya.musunuru/fvm/versions/2.10.3"
+export "FLUTTER_APPLICATION_PATH=/Users/aditya.musunuru/Documents/gitprojects/open_file_safe/example"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
-export "FLUTTER_TARGET=lib\main.dart"
+export "FLUTTER_TARGET=lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "FLUTTER_BUILD_NAME=1.0.0"
 export "FLUTTER_BUILD_NUMBER=1"
